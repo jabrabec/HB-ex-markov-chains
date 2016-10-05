a0f6b94aecf8d82a9d0585643352af2ea1b03d3c
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-05 13:00:39 -0700

    n-gram approach started

--- markov.py
+++ markov.py
@@ -17,7 +17,7 @@ def open_and_read_file(file_path):
 # input_path = "green-eggs.txt"
 # input_text = open_and_read_file(input_path)
 
-def make_chains(text_string):
+def make_chains(text_string, n):
     """Takes input text as string; returns _dictionary_ of markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -34,30 +34,31 @@ def make_chains(text_string):
     #create an empty dictionary called chains
     chains = {}
     # for each index in the range of text_string up to the 3nd to last character
-    for index in range(len(text_string) - 2):
+    for index in range(len(text_string) - n):
         # create a key that is a tuple made up of text_string
         # at current index and text_string at next index
-        two_word_key = text_string[index], text_string[index + 1]
-        # create a value to later assign to two_word_key that is
-        # equal to the text_string at the next next index
-        new_value = text_string[index + 2]
-        # if already exists a key of current two_word_key value in
-        # dictionary, perform following steps:
-        if chains.get(two_word_key):
-            # append new_value to existing value list for this key
-            chains[two_word_key].append(new_value)
-        # if there does not already exist a key of current two_word_key value in
-        # dictionary, assign key:value to two_word_key(tuple):third word(value)
-        else:
-            chains[two_word_key] = [new_value]
+        while index < n:
+            n_word_key = text_string[index], text_string[index + 1]
+            # create a value to later assign to n_word_key that is
+            # equal to the text_string at the next next index
+            new_value = text_string[index + 2]
+            # if already exists a key of current n_word_key value in
+            # dictionary, perform following steps:
+            if chains.get(n_word_key):
+                # append new_value to existing value list for this key
+                chains[n_word_key].append(new_value)
+            # if there does not already exist a key of current n_word_key value in
+            # dictionary, assign key:value to n_word_key(tuple):third word(value)
+            else:
+                chains[n_word_key] = [new_value]
 
     # printing instead of returning for now, for testing
     return chains
 
-def make_text(chains):
+def make_text(chains, n):
     """Takes dictionary of markov chains; returns random text."""
 
-    first_key = random.choice(chains.keys())
+    first_key = random.choice(chains.keys(n))
     first_key_values = chains[first_key]
     third_word = random.choice(first_key_values)
     temp_list = [first_key[0], first_key[1], third_word]
